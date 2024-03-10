f08bc5905b53d250d782bb1f4e3fe92372f2732d
brunolucy2023x <lucyb5147@gmail.com>
2024-03-10 05:42:03 -0700

    23

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Defines the AirBNB class."""
+"""Defines the Amenity class."""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represent an AirBNB.
+    """Represent an amenity.
 
     Attributes:
-        name (str): The name of the airbnb.
+        name (str): The name of the amenity.
     """
 
     name = ""
